616a6cc0ec137b296324800969d19ce87dc37155
Jattinger <jackson.attinger@gmail.com>
2025-03-06 12:33:24 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,5 +1,5 @@
 export const OWNER_NAME: string = `Jackson Attinger`;
-export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
+export const OWNER_DESCRIPTION: string = `A student @ UNC Chapel Hill`;
 
 export const AI_NAME: string = `a Fitness Trainer & Nutrionist`;
 export const AI_TONE: string = `[AI TONE]`;
