be741f362773bcb45de988df11ec70ff65e47730
Jattinger <jackson.attinger@gmail.com>
2025-03-06 12:32:45 -0500

    Update identity.ts

--- configuration/identity.ts
+++ configuration/identity.ts
@@ -1,6 +1,6 @@
 export const OWNER_NAME: string = `Jackson Attinger`;
 export const OWNER_DESCRIPTION: string = `[OWNER DESCRIPTION]`;
 
-export const AI_NAME: string = `a Fitness Trainer`;
+export const AI_NAME: string = `a Fitness Trainer & Nutrionist`;
 export const AI_TONE: string = `[AI TONE]`;
 export const AI_ROLE: string = `[AI ROLE]`;
