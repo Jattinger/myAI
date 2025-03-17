da196865cf6977e35b48ae44ddcd3a888f8c87ee
Jattinger <jackson.attinger@gmail.com>
2025-03-17 13:09:05 -0400

    Update chat.ts

--- configuration/chat.ts
+++ configuration/chat.ts
@@ -4,4 +4,4 @@ export const INITIAL_MESSAGE: string = `Hello, I'm ${AI_NAME}, ${OWNER_NAME}'s A
 export const DEFAULT_RESPONSE_MESSAGE: string = `Sorry, I can't generate a response right now. I'll be able to help you achieve your goals later.`;
 export const WORD_CUTOFF: number = 8000; // Number of words until bot says it needs a break
 export const WORD_BREAK_MESSAGE: string = `[WORD BREAK MESSAGE]`;
-export const HISTORY_CONTEXT_LENGTH: number = 7; // Number of messages to use for context when generating a response
+export const HISTORY_CONTEXT_LENGTH: number = 15; // Number of messages to use for context when generating a response
